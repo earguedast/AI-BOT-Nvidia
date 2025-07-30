7acb7288d63816fe42b084e018724bb1b306a3d9
earguedast <este.at@gmail.com>
2025-07-29 21:25:07 -0600

    Update full-pre-setup.sh

--- full-pre-setup.sh
+++ full-pre-setup.sh
@@ -61,7 +61,7 @@ fi
 
 # Install the NVIDIA CUDA Toolkit
 echo "Installing the NVIDIA CUDA Toolkit..."
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
 sudo apt-get update
 sudo apt-get install -y cuda-toolkit-12-9
