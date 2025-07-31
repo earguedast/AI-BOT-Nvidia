8d16fd66a283b4e754009cf91a26c92dceba18c2
earguedast <este.at@gmail.com>
2025-07-30 19:07:25 -0600

    Update quick-pre-setup.sh

--- quick-pre-setup.sh
+++ quick-pre-setup.sh
@@ -48,7 +48,11 @@ fi
 
 # Install the NVIDIA CUDA Toolkit
 echo "Installing the NVIDIA CUDA Toolkit..."
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
+sudo apt install -y linux-headers-$(uname -r) build-essential
+sudo apt update
+sudo apt install -y linux-headers-$(uname -r) build-essential dkms
+sudo apt update
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
 sudo apt-get update
 sudo apt-get install -y cuda-toolkit-12-9
