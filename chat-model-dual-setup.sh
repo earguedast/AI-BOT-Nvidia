98616a590aa0201c3703ee28584e6d4e38f5d895
earguedast <este.at@gmail.com>
2025-08-01 14:59:28 -0600

    Update chat-model-dual-setup.sh

--- chat-model-dual-setup.sh
+++ chat-model-dual-setup.sh
@@ -97,7 +97,7 @@ chat_model_1_vllm_container_args_base=(
     -p $chat_model_1_vllm_container_host_port:8000
     --runtime nvidia
     --gpus all
-    --tensor-parallel-size=$chat_model_1_vllm_gpu_count
+    --tensor-parallel-size $chat_model_1_vllm_gpu_count
     -v $HOME/ai_models:/ai_models
     --ipc=host
     $chat_model_1_vllm_container_image
@@ -172,7 +172,7 @@ chat_model_2_vllm_container_args_base=(
     -p $chat_model_2_vllm_container_host_port:8000
     --runtime nvidia
     --gpus all
-    --tensor-parallel-size=$chat_model_2_vllm_gpu_count
+    --tensor-parallel-size $chat_model_2_vllm_gpu_count
     -v $HOME/ai_models:/ai_models
     --ipc=host
     $chat_model_2_vllm_container_image
